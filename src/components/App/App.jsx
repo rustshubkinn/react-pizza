6ed946cb4a383928ed6ef165e59276d7c71f0b6d
Rustam Shubkin <rustshubkinn@mail.ru>
2021-08-19 21:32:12 +0600

    Update dependencies, global styles and add fonts
    Install classnames and prop-types dependencies in package.json and
    package-lock.json.
    Add Montserrat Font in index.html.
    Import global.scss in App.jsx and update styles with body properties.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from 'pages/Home/Home';
 
+import './global.scss';
+
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
