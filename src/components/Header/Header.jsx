07001f3ca69d0ab182a770e5b870d0b2a7e67d4c
Rustam Shubkin <rustshubkinn@mail.ru>
2021-08-20 16:25:29 +0600

    Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import Button from 'components/UI/Button/Button';
+import Logo from 'components/Logo/Logo';
+
+import { ReactComponent as CartSvg } from '../../img/shopping-cart.svg';
+
+import classes from './Header.module.scss';
+
+const Header = () => (
+  <div className={classes.header}>
+    <Logo />
+    <Button type="button" className={classes.cart_button}>
+      <span className={classes.total}>
+        520<span>&#8381;</span>
+      </span>
+      <span className={classes.cart}>
+        <CartSvg className={classes.svg} />3
+      </span>
+    </Button>
+  </div>
+);
+
+export default Header;
