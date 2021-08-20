42d3e1a4b172bf6fc88fadc8df08d576547d467c
Rustam Shubkin <rustshubkinn@mail.ru>
2021-08-20 16:29:12 +0600

    Implement Header component in Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,13 @@
-const Home = () => <h2>React Pizza</h2>;
+import Header from 'components/Header/Header';
+
+import classes from './Home.module.scss';
+
+const Home = () => (
+  <div className={classes.container}>
+    <header>
+      <Header />
+    </header>
+  </div>
+);
 
 export default Home;
