d8d570e75521aaf2b9800ac2310eca321287d740
Rustam Shubkin <rustshubkinn@mail.ru>
2021-08-20 16:24:50 +0600

    Append Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as SvgLogo } from '../../img/logo-pizza.svg';
+
+import classes from './Logo.module.scss';
+
+const Logo = () => (
+  <div className={classes.logo}>
+    <Link to="/">
+      <div className={classes.wrapper}>
+        <SvgLogo />
+        <div className={classes.text}>
+          <h1>React Pizza</h1>
+          <h2>самая вкусная пицца во вселенной</h2>
+        </div>
+      </div>
+    </Link>
+  </div>
+);
+
+export default Logo;
