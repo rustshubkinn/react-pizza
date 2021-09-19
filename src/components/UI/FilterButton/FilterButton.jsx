b6cc1159df6b03e836a374f5bd049c2e4bec3cc8
Rustam Shubkin <rustshubkinn@mail.ru>
2021-09-19 17:51:34 +0600

    Update FilterButton.jsx

--- src/components/UI/FilterButton/FilterButton.jsx
+++ src/components/UI/FilterButton/FilterButton.jsx
@@ -1,20 +1,26 @@
 import classNames from 'classnames';
-import { string } from 'prop-types';
+import { func, string } from 'prop-types';
 
 import classes from './FilterButton.module.scss';
 
-const FilterButton = ({ children, className }) => (
-  <button type="submit" className={classNames(classes.filter_btn, className)}>
+const FilterButton = ({ children, className, onClick }) => (
+  <button
+    type="submit"
+    onClick={onClick}
+    className={classNames(classes.filter_btn, className)}
+  >
     {children}
   </button>
 );
 
 FilterButton.propTypes = {
   children: string.isRequired,
+  onClick: func,
   className: string,
 };
 
 FilterButton.defaultProps = {
+  onClick: func,
   className: null,
 };
 
