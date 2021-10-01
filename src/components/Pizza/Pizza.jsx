bd3c9164df750e90373a3bf5a903db97aeb6f32b
Rustam Shubkin <rustshubkinn@mail.ru>
2021-10-01 14:53:02 +0600

    Add loading state to Pizza.jsx

--- src/components/Pizza/Pizza.jsx
+++ src/components/Pizza/Pizza.jsx
@@ -1,25 +1,32 @@
+import { useSelector } from 'react-redux';
 import { string, number, arrayOf, objectOf, shape } from 'prop-types';
 import classNames from 'classnames';
 
 import OptionsSelector from 'components/OptionsSelector/OptionsSelector';
+import Loader from 'components/UI/Loader/Loader';
 
 import classes from './Pizza.module.scss';
 
-const Pizza = ({ name, price, image, options, doughs, sizes, className }) => (
-  <div className={classNames(classes.pizza, className)}>
-    <img src={image} alt={`${name}`} />
-    <p>{name}</p>
-    <div className={classes.order_wrapper}>
-      <OptionsSelector
-        name={name}
-        price={price}
-        options={options}
-        doughs={doughs}
-        sizes={sizes}
-      />
+const Pizza = ({ name, price, image, options, doughs, sizes, className }) => {
+  const { loading } = useSelector((state) => state);
+
+  return (
+    <div className={classNames(classes.pizza, className)}>
+      <Loader loading={loading} />
+      <img src={image} alt={`${name}`} />
+      <p>{name}</p>
+      <div className={classes.order_wrapper}>
+        <OptionsSelector
+          name={name}
+          price={price}
+          options={options}
+          doughs={doughs}
+          sizes={sizes}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Pizza.propTypes = {
   name: string.isRequired,
